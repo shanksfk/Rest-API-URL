4b90dbbb1aa4d665ba0e16d7806700ab98f3e304
shanksfk <shanksfk@gmail.com>
2022-11-21 12:45:53 +0800

    first commit

--- thassment/urls.py
+++ thassment/urls.py
@@ -5,6 +5,9 @@ from .views import *
 urlpatterns = [
     path('sorted', viewPosts.as_view()),
     path('sor/<int:input>', viewPostDetails.as_view(), name='sorted'),
+    path('search/', filter_search, name='search'),
+    path('post-top/', sort_top, name='top'),
+
     re_path('^sor/(?P<title>.+)/$', viewPostDetails.as_view()),
     #     path('s', list_posts),
 ]
--- thassment/views.py
+++ thassment/views.py
@@ -92,3 +92,142 @@ class viewPostDetails(APIView):
             #             print(key)
 
             return Response(output)
+
+    def post(self, request, input, **kwargs):
+        posts_response = requests.get(
+            'https://jsonplaceholder.typicode.com/posts')
+        # queryset = posts_response
+        input=self.kwargs['id']
+        posts_data = json.loads(posts_response.text)
+
+        comments_response = requests.get(
+            'https://jsonplaceholder.typicode.com/comments')
+        comments_data = json.loads(comments_response.text)
+        # comments_data == response.json()
+
+        # count the comments
+        counts = Counter([d['postId'] for d in comments_data])
+
+        # add the counts to each post
+        # input = self.kwargs['keyword']
+        for d in posts_data:
+            d["number of comments"] = counts[d['id']]
+            d.pop("userId", None)
+        # posts_data[5]['number of comments'] = 60
+        # print(posts_data[5]['number of comments'])
+
+        # sort posts by number of comments in descending order
+        posts_data.sort(
+            key=lambda x: x['number of comments'], reverse=True)
+
+        output = {}
+        for field in posts_data:
+            if input == field['id']:
+                # print(field['id'])
+                output = field
+
+            # elif input == field['userId']:
+            #     # print(field['userId'])
+            #     output = field
+
+            else:
+                input = "{input}"
+                if input == field['title']:
+                    output = field
+                    print(field['title'])
+                else:
+                    print('none')
+            #     for key in field.keys():
+            #         if all(x in key for x in input):
+            #             print(key)
+
+            return Response(output)
+from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import render
+
+def sort_top(request):
+    
+        posts_response = requests.get(
+            'https://jsonplaceholder.typicode.com/posts')
+        posts_data = json.loads(posts_response.text)
+        # del posts_data['userId']
+        # print(posts.text)
+
+        comments_response = requests.get(
+            'https://jsonplaceholder.typicode.com/comments')
+        comments_data = json.loads(comments_response.text)
+        # comments_data == response.json()
+
+        # count the comments
+        counts = Counter([d['postId'] for d in comments_data])
+
+        # add the counts to each post
+        for d in posts_data:
+            d["numbercomments"] = counts[d['id']]
+            d.pop("userId", None)
+        # posts_data[5]['number of comments'] = 60
+        # print(posts_data[5]['number of comments'])
+
+        # sort posts by number of comments in descending order
+        posts_data.sort(
+            key=lambda x: x['numbercomments'], reverse=True)
+        return render(request, 'post-top.html', {'posts': posts_data})
+
+@csrf_exempt
+def filter_search(request):
+    posts_response = requests.get(
+        'https://jsonplaceholder.typicode.com/posts')
+    # queryset = posts_response
+    
+    posts_data = json.loads(posts_response.text)
+    # print(posts_data)
+
+    comments_response = requests.get(
+        'https://jsonplaceholder.typicode.com/comments')
+    comments_data = json.loads(comments_response.text)
+
+    counts = Counter([d['postId'] for d in comments_data])
+
+    # add the counts to each post
+    # input = self.kwargs['keyword']
+    for d in posts_data:
+        d["number of comments"] = counts[d['id']]
+        d.pop("userId", None)
+    # posts_data[5]['number of comments'] = 60
+    # print(posts_data[5]['number of comments'])
+
+    # sort posts by number of comments in descending order
+    posts_data.sort(
+        key=lambda x: x['number of comments'], reverse=True)
+
+
+    output = {}
+    post=[]
+
+
+    if request.method=='POST':
+        input=request.POST.get('input')
+        print(type(input))
+        
+        for field in posts_data:
+            if isinstance(input, int):
+                if (int(input) == field['id']):
+                    print(True)
+                    output['post'] = field
+                    print(output)
+
+                elif (input==field['userId']):
+                    print('userid')
+
+            elif field['title'].find(input):
+                output['post'] = field
+                print('found title')
+                print(field['title'])
+            else:
+                print('not found')
+
+
+        return render(request, 'post.html', {'post':output})
+
+    else:
+        return render(request, 'post.html', {'posts': posts_data})
